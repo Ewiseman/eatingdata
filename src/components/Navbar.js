39f5cc7d66f9898aa2b242c41da9abb4f4090567
Eric Wiseman <ewiseman1.com>
2021-03-18 08:24:48 -0600

    removed scollbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import RecipesList from "./recipes/RecipesList";
 import Landing from "./Landing";
-import ScrollToTop from "./ScrollToTop";
+// import ScrollToTop from "./ScrollToTop";
 import CookbooksList from "./cookbooks/CookbooksList";
 import Recipe from "./recipes/Recipe";
 import Cookbook from "./cookbooks/Cookbook";
@@ -68,7 +68,7 @@ const Navbar = ({ fixed }) => {
           </div>
         </nav>
       </>
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <Switch>
         <Route exact path="/">
           <Landing />
